6bda4a6646b89aea3cb4db6df5bbb621af4e32e5
ifanwu <ifanwu@connect-172-31-26-78.vpn.tamu.edu>
2021-12-17 17:22:52 -0600

    separate evaluation from kmer construction/training

---
+++ evaluate_models.py
@@ -0,0 +1,33 @@
+import pickle
+from utility import readFasta, get_label_list, get_accu_f1
+from KmerConstruct import KmerConstruct
+import pandas as pd
+
+
+#pick a mock and do prediction on eg. genus
+new_KmerConstruct = pickle.load(open("../KmerConstruct_4mer.pickle","rb" ))
+model= pickle.load(open("../KmerNBmodel_4mer.pickle","rb" ))
+
+
+#construct test_X (k-mer frequency table) from mock daxxwtax
+label_mock2,seq_mock2=readFasta('../tax-credit-data/data/mock-community/mock-2/dna-sequences.fasta') 
+taxonomy_table=pd.read_csv('../tax-credit-data/data/mock-community/mock-2/matched-sequence-taxonomies.tsv',sep='\t')
+labels=taxonomy_table['Standard Taxonomy'].tolist()
+answers_=get_label_list(labels,type_='g__')
+
+KmerConstruct_mock2=KmerConstruct(seq_mock2,k=4,n_threads=1)
+KmerConstruct_mock2.features=new_KmerConstruct.features #map the k-mer features to those of the classifier
+KmerConstruct_mock2.constuct_feature_table()
+
+X_test=KmerConstruct_mock2.X
+#map the features to those of the classifiers'
+
+
+y=answers_
+y_pred = model.predict(X_test) 
+get_accu_f1(y_pred,y)
+
+
+
+#plot
+
--- naive_bayes_larger_k.py
+++ naive_bayes_larger_k.py
@@ -76,45 +76,6 @@ if __name__ == '__main__':
 
 
 
-    """
-
-    #pick a mock and do prediction on eg. genus
-    #KmerConstruct_4mer = pickle.load(open("../KmerConstruct_4mer.pickle","rb" ))
-    
-    X=new_KmerConstruct.X
-    y=genus_labels[0:20000]
-    model = MultinomialNB()
-    model.fit(X, y)       
-    y_pred = model.predict(X)
-            
-    print("Training error (based on accuracy) is: ",accuracy_score(y_pred,y)) 
-    
-    #construct test_X (k-mer frequency table) from mock data
-    
-    label_mock2,seq_mock2=readFasta('../tax-credit-data/data/mock-community/mock-2/dna-sequences.fasta') 
-    taxonomy_table=pd.read_csv('../tax-credit-data/data/mock-community/mock-2/matched-sequence-taxonomies.tsv',sep='\t')
-    labels=taxonomy_table['Standard Taxonomy'].tolist()
-    answers_=get_label_list(labels,type_='g__')
-    
-    KmerConstruct_mock2=KmerConstruct(seq_mock2,k=4,n_threads=1)
-    KmerConstruct_mock2.features=new_KmerConstruct.features #map the k-mer features to those of the classifier
-    KmerConstruct_mock2.constuct_feature_table()
-    
-    X_test=KmerConstruct_mock2.X
-    #map the features to those of the classifiers'
-    
-    
-    y=answers_
-    y_pred = model.predict(X_test) 
-    get_accu_f1(y_pred,y)
-    
-    
-    
-    #plot
-
-    """
-
-
 
 #some test data and helpers
 

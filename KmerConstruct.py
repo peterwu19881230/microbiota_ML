c262bc0ef074a594961b9753b4bbfefaf5edbbcc
ifanwu <ifanwu@peters-M1-macbook.local>
2021-12-22 21:19:36 -0600

    reduce RAM requirement

--- KmerConstruct.py
+++ KmerConstruct.py
@@ -4,6 +4,8 @@ import numpy as np
 from tqdm import tqdm
 import multiprocessing
 from multiprocessing import Pool, Process
+import itertools  
+import scipy.sparse as sparse
 
 
 class KmerConstruct():
@@ -13,6 +15,7 @@ class KmerConstruct():
         self.ss_kmers=None
         self.k = k
         self.X = None
+        self.X_packed=None
         self.X_first_j=None
         self.n_threads=n_threads
         self.features=None
@@ -60,18 +63,17 @@ class KmerConstruct():
         #flatten the kmers: list of lists -> list
         print('==Getting all unique kmers==')
         flattened_ss_kmers=[s_kmer for s_kmers in tqdm(ss_kmers) for s_kmer in s_kmers]
-        return list(set(flattened_ss_kmers))
+        return sorted(list(set(flattened_ss_kmers))) #it has to be sorted. Otherwise set gives different orders at each run
     
-    def fill_feature(self,batch):
-        X_batch=np.zeros((batch,len(self.features)),dtype='int32')
-        for i in tqdm(range(batch)):
-            for j in range(len(self.features)):   
-                X_batch[i,j]=self.ss_kmers[i].count(self.features[j])
-        
-        return X_batch
+    def fill_feature(self,batch_index):
+        batch_values=[0]*len(batch_index)*len(self.features)
+        for j,i in tqdm(zip(range(len(batch_index)),batch_index),total=len(batch_index)):
+            for kmer in self.ss_kmers[i]: 
+                batch_values[j*len(self.features)+self.features.index(kmer)]+=1          
+        return batch_values
     
     
-    def constuct_feature_table(self,j_=None): #for different samples, merge their k-mer frequencies in one feature table. j: get the most discriminative kmers (features)
+    def constuct_feature_table(self): #for different samples, merge their k-mer frequencies in one feature table. j: get the most discriminative kmers (features)
            
         if self.X is None:
             self.ss_kmers=self.build_kmer_list(self.ss,self.k)
@@ -91,31 +93,38 @@ class KmerConstruct():
                     
                     if keep: new_features.append(self.features[i])
                 
-                self.features=new_features
-                    
+                self.features=new_features 
+                                  
             
             print('==constructing the full feature table==')
             
-            self.X=[]
+
+            #compute rows, cols, values -> convert to a sparse matrix later              
+            rows_ = [i for i in range(len(self.ss_kmers))]
+            rows = list(itertools.chain.from_iterable(itertools.repeat(i, len(self.features)) for i in rows_))    
+            cols=[j for j in range(len(self.features))]*len(self.ss_kmers)
             
             if self.n_threads==1:
-                self.X=self.fill_feature(len(self.ss))
+                values=self.fill_feature(rows_)
             else:
-                processes=[]
-                num, div = len(self.ss_kmers), self.n_threads
-                batches=[num // div + (1 if x < num % div else 0)  for x in range (div)]
-                
+                values=[]
+                batch_size=len(self.ss_kmers)//self.n_threads
+                batch_index_list = [rows_[i:i + batch_size] for i in range(0, len(rows_), batch_size)]  
                 pool = Pool(self.n_threads)
-                result = pool.map(self.fill_feature, batches)
+                result = pool.map(self.fill_feature, batch_index_list)
                                 
                 for i in range(len(result)):
-                    self.X.extend(result[i])
-                
-                
+                    values.extend(result[i]) 
+
+            self.X_packed=(rows,cols,values)
+            
         
-                    
+    def unpack_feature_table(self,j_=None):
+        rows,cols,values=self.X_packed
+        print('==unpacking feature table==')         
+        self.X=sparse.csr_matrix((values, (rows, cols)), shape=(len(self.ss_kmers), len(self.features))).toarray()
         
-        if j_ is not None:
+        if  j_ is not None:
             #calculate no. of 0s for each column and sort by desc order
             X_t=self.X.T
             sum_zeros=[]

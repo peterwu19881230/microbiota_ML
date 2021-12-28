2a51d9b11b435c9e011cf0686a0a64fbcf88a268
ifanwu <ifanwu@connect-172-31-27-46.vpn.tamu.edu>
2021-12-28 10:48:14 -0600

    corrected model. now performs identically to the sklearn's

--- multinomialNB.py
+++ multinomialNB.py
@@ -9,9 +9,8 @@ class multinomialNB():
         self.uniq_y=None
         
     def train(self):
-        self.all_p_yi=[]
+        self.all_prior_yi=[]
         self.all_p_feature_given_yi=[]
-        N=np.sum(self.X_train)
         n_features=self.X_train.shape[1]
         self.uniq_y=sorted(list(set(self.y_train)))
         
@@ -21,14 +20,15 @@ class multinomialNB():
             ind=[i for i, _y in enumerate(self.y_train) if _y == yi]
             
             #calculate prior probabilities for each class
-            self.all_p_yi.append(len(ind)/len(self.y_train))
+            self.all_prior_yi.append(len(ind)/len(self.y_train))
                
             #calculate p(a kmer|a class) for each class
             X_yi=self.X_train[ind]
+            N_yi=np.sum(X_yi)
             p_feature_given_yi=[]
             for i in range(X_yi.shape[1]):
                 feature_vec=X_yi[:,i].A
-                p_feature_given_yi.append((np.sum(feature_vec)+self.alpha)/(N+self.alpha*n_features))
+                p_feature_given_yi.append((np.sum(feature_vec)+self.alpha)/(N_yi+self.alpha*n_features))
             
             self.all_p_feature_given_yi.append(p_feature_given_yi)
         print('\n')
@@ -41,7 +41,7 @@ class multinomialNB():
         for test_xi in tqdm(X_test):
             log_prob_all_y=[]
             for i,yi in enumerate(self.uniq_y): #for each class    
-                log_prob_yi=np.log(self.all_p_yi[i])    
+                log_prob_yi=np.log(self.all_prior_yi[i])    
                 for j,p_feature_given_yi in enumerate(self.all_p_feature_given_yi[i]): #for each feature
                     freq=test_xi[j]
                     if freq==0: freq+=self.alpha #not sure if this is the best smoothing

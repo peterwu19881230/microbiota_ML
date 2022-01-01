953a3f4ad19b1072113a00524b46eb62c40943bf
ifanwu <ifanwu@peters-M1-macbook.local>
2022-01-01 17:49:57 -0600

    correct minor mistake

--- train_NB_models.py
+++ train_NB_models.py
@@ -6,6 +6,7 @@ import numpy as np
 if __name__ == '__main__':
     
     ks=[4,6,7,8,9,10,11,12,14,16,18,32,64,100]
+    ks=[4]
     
     with open('load_silva_data.py') as infile:
         exec(infile.read()) #ss, genus_labels are given here
@@ -21,7 +22,8 @@ if __name__ == '__main__':
             model=dict_input_multinomialNB(ls_dict_X_train=new_KmerConstruct.all_freq_dict,n_features=len(new_KmerConstruct.features),y_train=y,alpha=0.1,include_prior=True) 
             model.train()
             
+            model_filename='../NBmodel_'+ str(k) + 'mer.pickle'
             print('saving the file...')
-            with open(filename,'wb') as handle:
+            with open(model_filename,'wb') as handle:
                 pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
                 print('kmer freq for k=',k,' is done')

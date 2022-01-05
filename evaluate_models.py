724b285686b5d85f1e9826c2398eed20f7934e94
ifanwu <ifanwu@connect-172-31-32-209.vpn.tamu.edu>
2022-01-05 11:56:15 -0600

    update and show the result

--- evaluate_models.py
+++ evaluate_models.py
@@ -9,7 +9,7 @@ from os.path import exists
 new_KmerConstruct = pickle.load(open("../KmerConstruct_4mer.pickle","rb" ))
 
 #construct test_X (k-mer frequency table) from mock daxxwtax
-label_mock2,seq_mock2=readFasta('../tax-credit-data/data/mock-community/mock-2/expected-sequences.fasta') 
+label_mock2,seq_mock2=readFasta('../tax-credit-data/data/mock-community/mock-2/dna-sequences.fasta') 
 taxonomy_table=pd.read_csv('../tax-credit-data/data/mock-community/mock-2/matched-sequence-taxonomies.tsv',sep='\t')
 labels=taxonomy_table['Standard Taxonomy'].tolist()
 answers_=get_label_list(labels,type_='g__')
@@ -29,11 +29,53 @@ for k in ks:
         y_pred = model.test(KmerConstruct_mock2.all_freq_dict) 
         accu,f1=get_accu_f1(y_pred,y_test)
         
-        print("Accuracy= ",accu," for k=",k) 
-        print("F1= ",f1," for k=",k)
+        #print("Accuracy= ",accu," for k=",k) #grace won't print this
+        #print("F1= ",f1," for k=",k) #grace won't print this
 
 
 #plot
 
-#when k=10: Accuracy=  0.3953488372093023 F1=  0.5666666666666667
-#when k=16: Accuracy=  0.43902439024390244 F1=  0.6101694915254238
+#k=4 Accuracy=  0.2916666666666667 F1=  0.45161290322580644
+
+#k=6 Accuracy=  0.37777777777777777 F1=  0.5483870967741935
+
+#k=7 Accuracy=  0.38636363636363635 F1=  0.5573770491803278
+
+#k=8 Accuracy=  0.38636363636363635 F1=  0.5573770491803278
+
+#k=9 Accuracy=  0.38636363636363635 F1=  0.5573770491803278
+
+#k=10 Accuracy=  0.3953488372093023 F1=  0.5666666666666667
+
+#k=11 Accuracy=  0.4146341463414634 F1=  0.5862068965517241
+
+#k=12 Accuracy=  0.45 F1=  0.6206896551724138
+
+#k=14 Accuracy=  0.43902439024390244 F1=  0.6101694915254238
+
+#k=16 Accuracy=  0.43902439024390244 F1=  0.6101694915254238
+
+#k=18 Accuracy=  0.42857142857142855 F1=  0.6
+
+#k=32 Accuracy=  0.4186046511627907 F1=  0.5901639344262295
+
+#k=64 Accuracy=  0.4090909090909091 F1=  0.5806451612903226
+
+#k=100 Accuracy=  0.4090909090909091 F1=  0.5806451612903226
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

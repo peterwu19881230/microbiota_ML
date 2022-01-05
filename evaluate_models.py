2ba801cb6f15acbf51b25b679a108ef140500849
ifanwu <ifanwu@connect-172-31-32-209.vpn.tamu.edu>
2022-01-05 10:34:52 -0600

    dna-sequences -> expected-sequences

--- evaluate_models.py
+++ evaluate_models.py
@@ -9,13 +9,12 @@ from os.path import exists
 new_KmerConstruct = pickle.load(open("../KmerConstruct_4mer.pickle","rb" ))
 
 #construct test_X (k-mer frequency table) from mock daxxwtax
-label_mock2,seq_mock2=readFasta('../tax-credit-data/data/mock-community/mock-2/dna-sequences.fasta') 
+label_mock2,seq_mock2=readFasta('../tax-credit-data/data/mock-community/mock-2/expected-sequences.fasta') 
 taxonomy_table=pd.read_csv('../tax-credit-data/data/mock-community/mock-2/matched-sequence-taxonomies.tsv',sep='\t')
 labels=taxonomy_table['Standard Taxonomy'].tolist()
 answers_=get_label_list(labels,type_='g__')
 
-KmerConstruct_mock2=KmerConstruct(seq_mock2,k=4,n_threads=1)
-KmerConstruct_mock2.construct_all_freq_dict()
+
 y_test=answers_
 
 #map the features to those of the classifiers'
@@ -25,9 +24,13 @@ for k in ks:
     filename='../NBmodel_'+ str(k) + 'mer.pickle'
     if exists(filename):        
         model = pickle.load(open(filename,"rb" ))
+        KmerConstruct_mock2=KmerConstruct(seq_mock2,k=k,n_threads=1)
+        KmerConstruct_mock2.construct_all_freq_dict()
         y_pred = model.test(KmerConstruct_mock2.all_freq_dict) 
         get_accu_f1(y_pred,y_test)
 
 
 #plot
 
+#when k=10: Accuracy=  0.3953488372093023 F1=  0.5666666666666667
+#when k=16: Accuracy=  0.43902439024390244 F1=  0.6101694915254238

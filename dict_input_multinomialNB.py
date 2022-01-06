e51d98ce94054d171ab07d2374fa01a6b5dd5fce
ifanwu <ifanwu@peters-M1-macbook.local>
2022-01-05 19:16:46 -0600

    1.reduced size of model 2.reduced testing time

--- dict_input_multinomialNB.py
+++ dict_input_multinomialNB.py
@@ -13,21 +13,23 @@ class dict_input_multinomialNB():
         self.alpha=alpha
         self.uniq_y=None
         self.include_prior=include_prior
+        self.kmer_not_present_logp=[]
         
     def train(self):
-        self.all_prior_yi=[]
-        self.all_p_feature_given_yi=[]
+        self.all_log_prior_yi=[]
+        self.all_logp_feature_given_yi=[]
         self.uniq_y=sorted(list(set(self.y_train)))
         self.N_yi_ls=[]
         
         print('==Training...==')
-        for yi in tqdm(self.uniq_y):
+        for i_,yi in tqdm(enumerate(self.uniq_y)):
             
             ind=[i for i, _y in enumerate(self.y_train) if _y == yi]
             
             #calculate prior probabilities for each class
             if self.include_prior:
-                self.all_prior_yi.append(len(ind)/len(self.y_train))
+                prob=len(ind)/len(self.y_train)
+                self.all_log_prior_yi.append(np.log(prob))
                
             #calculate p(a kmer|a class) for each class
             ls_dict_X_train_yi=[self.ls_dict_X_train[i] for i in ind]
@@ -46,17 +48,24 @@ class dict_input_multinomialNB():
                         
             self.N_yi_ls.append(N_yi)
             
-            p_feature_given_yi={}
+            logp_feature_given_yi={}
             for kmer in sum_feature_vec_dict:
-                p_feature_given_yi[kmer]=(sum_feature_vec_dict[kmer]+self.alpha)/(N_yi+self.alpha*self.n_features)            
+                prob=(sum_feature_vec_dict[kmer]+self.alpha)/(N_yi+self.alpha*self.n_features) 
+                logp_feature_given_yi[kmer]=np.log(prob)           
             
-            self.all_p_feature_given_yi.append(p_feature_given_yi)
-            self.all_unique_kmers=sorted(list(set([key for dict_ in self.all_p_feature_given_yi for key in dict_])))
+            self.all_logp_feature_given_yi.append(logp_feature_given_yi)
+            self.all_unique_kmers=sorted(list(set([key for dict_ in self.all_logp_feature_given_yi for key in dict_])))
+            
+            prob=(self.alpha)/(self.N_yi_ls[i_]+self.alpha*self.n_features)
+            self.kmer_not_present_logp.append(np.log(prob))
+            
+        self.ls_dict_X_train='-removed to save space-'
             
         print('\n')
         print('-------------')
         print('training done')
         print('-------------')
+        
         
     def test(self,ls_dict_X_test):
         
@@ -65,19 +74,19 @@ class dict_input_multinomialNB():
             log_prob_all_y=[]
             for i,yi in enumerate(self.uniq_y): #for each class    
                 if self.include_prior: 
-                    log_prob_yi=np.log(self.all_prior_yi[i]) 
+                    log_prob_yi=self.all_log_prior_yi[i]
                 else:
                     log_prob_yi=0
                    
                 for kmer in test_xi:
                     freq=test_xi[kmer]
                     
-                    if kmer not in self.all_p_feature_given_yi[i]:
-                        prob=(self.alpha)/(self.N_yi_ls[i]+self.alpha*self.n_features)
+                    if kmer not in self.all_logp_feature_given_yi[i]:
+                        logp=self.kmer_not_present_logp[i]
                     else:
-                        prob=self.all_p_feature_given_yi[i][kmer]
+                        logp=self.all_logp_feature_given_yi[i][kmer]
                         
-                    log_prob_yi+=freq*np.log(prob) 
+                    log_prob_yi+=freq*logp
                                 
                 log_prob_all_y.append(log_prob_yi)
             

769b3178f89a50254e3cf132e2b02625111368a2
ifanwu <ifanwu@connect-172-31-27-158.vpn.tamu.edu>
2022-01-20 20:31:34 -0600

    parallelized

--- compute_kmer_frequencies.py
+++ compute_kmer_frequencies.py
@@ -14,26 +14,67 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
-
 import pickle
+from multiprocessing import Pool
+
+
+
+
+def constuct_(k):
+	
+	#can't just do the following. Don't know why
+	"""
+	with open('load_silva_data.py') as infile:
+		exec(infile.read())
+	"""
+	
+	from utility import readFasta, get_label_list
+
+	#load data from silva
+	#there are sequences lost after truncation. Retrieve the id indices and applied that on the taxonomy labels
+	label_silva_138_99_V4_truncated,seq_silva_138_99_V4_truncated=readFasta('../silva/silva138_99_truncated.fasta') 
+	label_silva_138_99_V4,seq_silva_138_99_V4=readFasta('../silva/silva138_99.fasta') 
+
+	indices_after_truncation=[]
+	curr_index=0
+	for i in range(len(label_silva_138_99_V4_truncated)):
+		while label_silva_138_99_V4_truncated[i]!=label_silva_138_99_V4[curr_index]:
+			curr_index+=1
+		indices_after_truncation.append(curr_index)    
+			
+	
+	ss=seq_silva_138_99_V4_truncated
+
+
+	#read taxonomy data
+	import pandas as pd
+	taxonomy_table=pd.read_csv('../silva/taxonomy.tsv',sep='\t')
+	labels=taxonomy_table['Taxon'].tolist()
+
+
+	#get genus as labels
+	genus_labels=get_label_list(labels,type_='g__')
+	genus_labels=np.array(genus_labels)
+	genus_labels=genus_labels[indices_after_truncation]
+	
+	#construct kmer feature table 
+	filename='../kmer_feature_objects/KmerFeatureEng_'+ str(k) + 'mer.pickle'
+	if not exists(filename):        
+		#construct kmer feature table
+		new_KmerFeatureEng=KmerFeatureEng(genus_labels,ss,k,n_threads=1)
+		new_KmerFeatureEng.construct_all_freq_dict()
+		print('saving the file...')
+		with open(filename,'wb') as handle:
+			pickle.dump(new_KmerFeatureEng, handle, protocol=pickle.HIGHEST_PROTOCOL)
+		print('kmer freq for k=',k,' is done')
 
 
 if __name__ == '__main__':
-    with open('load_silva_data.py') as infile:
-        exec(infile.read())
+	ks=[4,6,7,8,9,10,11,12,14,16,18,32,64,100]
+	n_threads=len(ks)
+	pool = Pool(n_threads)
+	pool.map(constuct_,ks)
 
-    ks=[4,6,7,8,9,10,11,12,14,16,18,32,64,100]
-    
-    #construct kmer feature table 
-    for k in ks:
-        filename='../kmer_feature_objects/KmerFeatureEng_'+ str(k) + 'mer.pickle'
-        if not exists(filename):        
-            #construct kmer feature table
-            new_KmerFeatureEng=KmerFeatureEng(genus_labels,ss,k,n_threads=1)
-            new_KmerFeatureEng.construct_all_freq_dict()
-            print('saving the file...')
-            with open(filename,'wb') as handle:
-                pickle.dump(new_KmerFeatureEng, handle, protocol=pickle.HIGHEST_PROTOCOL)
-            print('kmer freq for k=',k,' is done')
-        
+
+	
 

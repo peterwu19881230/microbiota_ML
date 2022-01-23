74d7d8c2600c3df1d4efa4da260e2d3d430147e0
ifanwu <ifanwu@connect-172-31-34-23.vpn.tamu.edu>
2022-01-22 21:45:01 -0600

    add new file

--- compute_discrit_kmer_feature_obj.py
+++ compute_discrit_kmer_feature_obj.py
@@ -1,11 +1,30 @@
 #construct discrit mixed kmer obj based on discrit kmers from all kmers mixed
-
+from test_data import * #ss, genus_labels are given here
 import pickle
 
-new_KmerFeatureEng=pickle.load(open('../kmer_feature_objects/KmerFeatureEng_all_mer.pickle','rb'))
-new_KmerFeatureEng.construct_discrit_freq_dict(reamin=.5)
+all_freq_dict=pickle.load(open('../kmer_feature_objects/mixed_feature_lists.pickle','rb')) 
 
 
-with open('../kmer_feature_objects/new_KmerFeatureEng_all_mer_discrit_0.5.pickle','wb') as handle:
-    pickle.dump(new_KmerFeatureEng, handle, protocol=pickle.HIGHEST_PROTOCOL)                
+all_KmerFeatureEng=KmerFeatureEng(genus_labels,ss,k=None)
+all_KmerFeatureEng.all_freq_dict=all_freq_dict
+
+
+ss_kmers=[]
+for dict_ in all_freq_dict:
+    for key in dict_:
+        for i in range(dict_[key]):
+            ss_kmers.append(key)
+all_KmerFeatureEng.ss_kmers=ss_kmers
+
+all_KmerFeatureEng.features=all_KmerFeatureEng.get_all_uniq_kmers(all_KmerFeatureEng.ss_kmers)
+
+
+remain=.5
+all_KmerFeatureEng.construct_discrit_freq_dict(remain=remain)
+discrit_freq_dict=all_KmerFeatureEng.discrit_freq_dict
+
+
+
+with open('../kmer_feature_objects/new_KmerFeatureEng_all_mer_discrit_'+str(remain)+'.pickle','wb') as handle:
+    pickle.dump(discrit_freq_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)                
     print('done')   
